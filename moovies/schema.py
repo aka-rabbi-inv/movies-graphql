f37385dacf16f154a275b3859af456c90b41edcd
aka-rabbi-inv <87089810+aka-rabbi-inv@users.noreply.github.com>
2023-10-03 14:52:51 +0600

    feat: first create mutation

--- moovies/schema.py
+++ moovies/schema.py
@@ -1,9 +1,14 @@
 import graphene
 from moovies.api.schema import Query as MovieQuery
+from moovies.api.schema import Mutation as MovieMutation
 
 
 class Query(MovieQuery, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(MovieMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
